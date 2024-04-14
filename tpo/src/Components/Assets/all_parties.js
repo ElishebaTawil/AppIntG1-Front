64409e3b20cdf77ee8996e3ef1da4f8e8a3451a6
maxirosso <maxrosso@uade.edu.ar>
2024-04-14 17:02:15 -0300

    fff

---
+++ tpo/src/Components/Assets/all_parties.js
@@ -0,0 +1,230 @@
+import bali from './bali.webp';
+import kika from './kika.jpg';
+import terrazas from './terrazas.jpg';
+import pacha from './pacha.jpg';
+import ibiza from './ibiza.jpeg';
+import samba from './samba.jpg';
+import disco from './disco.jpg';
+import reggae from './reggae.jpg';
+import electro from './electro.jpg';
+import jazz from './jazz.jpg';
+import revival from './revival.jpg';
+import hiphop from './hiphop.jpg';
+import rock from './rock.jpg';
+import latin from './latin.jpg';
+import karaoke from './karaoke.jpg';
+import carnival from './carnival.jpg';
+import funky_town from './funky_town.jpg';
+import masquerade from './masquerade.jpg';
+import salsa from './salsa.jpg';
+import techno from './techno.jpg';
+import duki from './duki.jpg';
+import badBunny from './badBunny.webp';
+import tini from './tini.webp';
+import karolG from './karolg1.jpg';
+import daddyYankee from './daddyyankee.avif';
+import lali from './lali.avif';
+
+
+let all_parties = [
+    {
+        id: 1,
+        name: "Bali",
+        image: bali,
+        new_price: 100,
+        old_price: 200,
+        category: "recintos"
+    },
+    {
+        id: 2,
+        name: "Kika",
+        image: kika,
+        new_price: 150,
+        old_price: 250,
+        category: "recintos"
+    },
+    {
+        id: 3,
+        name: "Terrazas joda",
+        image: terrazas,
+        new_price: 120,
+        old_price: 220,
+        category: "recintos"
+    },
+    {
+        id: 4,
+        name: "Pacha",
+        image: pacha,
+        new_price: 90,
+        old_price: 180,
+        category: "recintos"
+    },
+    {
+        id: 5,
+        name: "Ibiza Beach Party",
+        image: ibiza,
+        new_price: 180,
+        old_price: 280,
+        category: "recintos"
+    },
+    {
+        id: 6,
+        name: "Samba Nights",
+        image: samba,
+        new_price: 200,
+        old_price: 300,
+        category: "recintos"
+    },
+    {
+        id: 7,
+        name: "Disco Fever",
+        image: disco,
+        new_price: 140,
+        old_price: 240,
+        category: "recintos"
+    },
+    {
+        id: 8,
+        name: "Reggae Rhythms",
+        image: reggae,
+        new_price: 120,
+        old_price: 220,
+        category: "recintos"
+    },
+    {
+        id: 9,
+        name: "Electro Lounge",
+        image: electro,
+        new_price: 160,
+        old_price: 260,
+        category: "recintos"
+    },
+    {
+        id: 10,
+        name: "Jazz Club",
+        image: jazz,
+        new_price: 130,
+        old_price: 230,
+        category: "recintos"
+    },
+    {
+        id: 11,
+        name: "Rooftop Revival",
+        image: revival,
+        new_price: 190,
+        old_price: 290,
+        category: "recintos"
+    },
+    {
+        id: 12,
+        name: "Hip Hop Hangout",
+        image: hiphop,
+        new_price: 110,
+        old_price: 210,
+        category: "recintos"
+    },
+    {
+        id: 13,
+        name: "Rockin' Raucous",
+        image: rock,
+        new_price: 170,
+        old_price: 270,
+        category: "recintos"
+    },
+    {
+        id: 14,
+        name: "Latin Fiesta",
+        image: latin,
+        new_price: 150,
+        old_price: 250,
+        category: "recintos"
+    },
+    {
+        id: 15,
+        name: "Karaoke Kingdom",
+        image: karaoke,
+        new_price: 100,
+        old_price: 200,
+        category: "recintos"
+    },
+    {
+        id: 16,
+        name: "Carnival Carnival",
+        image: carnival,
+        new_price: 220,
+        old_price: 320,
+        category: "recintos"
+    },
+    {
+        id: 17,
+        name: "Funky Town",
+        image: funky_town,
+        new_price: 170,
+        old_price: 270,
+        category: "recintos"
+    },
+    {
+        id: 18,
+        name: "Mystic Masquerade",
+        image: masquerade,
+        new_price: 200,
+        old_price: 300,
+        category: "recintos"
+    },
+    {
+        id: 19,
+        name: "Salsa Soiree",
+        image: salsa,
+        new_price: 140,
+        old_price: 240,
+        category: "recintos"
+    },
+    {
+        id: 20,
+        name: "Techno Temple",
+        image: techno,
+        new_price: 180,
+        old_price: 280,
+        category: "recintos"
+    },
+
+    {
+        id: 21,
+        name: "Duki",
+        image: duki,
+        category: "artistas"
+
+    },
+    {
+        id: 22,
+        name: "Bad Bunny",
+        image: badBunny,
+        category: "artistas"
+    },
+    {
+        id: 23,
+        name: "Tini",
+        image: tini,
+        category: "artistas"
+    },
+    {
+        id: 24,
+        name: "Karol G",
+        image: karolG,
+        category: "artistas"
+    },
+    {
+        id: 25,
+        name: "Daddy Yankee",
+        image: daddyYankee,
+        category: "artistas"
+    },
+    {
+        id: 26,
+        name: "Lali",
+        image: lali,
+        category: "artistas"
+    }
+    
+];
+export default all_parties;
