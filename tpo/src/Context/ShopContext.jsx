068f1c490ccdcb07db5ffae6453c73db6f2b1e5e
SebastianSosa12 <sasc1999@hotmail.com>
2024-04-18 20:57:59 -0300

    Agregada la funcion para que al entrar a un evento se ajuste bien la pantalla. Agregada funcionalidades del Login, no terminado.

--- tpo/src/Context/ShopContext.jsx
+++ tpo/src/Context/ShopContext.jsx
@@ -1,58 +1,65 @@
-import React,{createContext} from 'react'
-import all_parties from '../Components/Assets/all_parties';
-import {useState} from 'react';
+import React, { createContext } from "react";
+import all_parties from "../Components/Assets/all_parties";
+import { useState } from "react";
 
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
-    let cart ={};
-    for (let index = 0 ; index < all_parties.length + 1; index++){
-        cart[index] = 0;
-    }
-    return cart;
-}
+  let cart = {};
+  for (let index = 0; index < all_parties.length + 1; index++) {
+    cart[index] = 0;
+  }
+  return cart;
+};
 
 const ShopContextProvider = (props) => {
-    const [cartItems,setCartItems] = useState(getDefaultCart());
-    
-
-    const addToCart = (itemId) => {
-    setCartItems((prev)=> ({...prev,[itemId]: prev[itemId] + 1}));
+  const [cartItems, setCartItems] = useState(getDefaultCart());
+  const [user, setUser] = useState({ name: "nombre", role: "admin" });
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     console.log(cartItems);
+  };
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = all_parties.find((party) => party.id === Number(item));
+        totalAmount += itemInfo.new_price * cartItems[item];
+      }
     }
-    const removeFromCart = (itemId) => {
-        setCartItems((prev)=> ({...prev,[itemId]: prev[itemId] - 1}));
-        }
+    return totalAmount; // Move return statement outside of the loop
+  };
 
-        const getTotalCartAmount = () => {
-            let totalAmount = 0;
-            for (const item in cartItems) {
-                if (cartItems[item] > 0) {
-                    let itemInfo = all_parties.find((party) => party.id === Number(item));
-                    totalAmount += itemInfo.new_price * cartItems[item];
-                }
-            }
-            return totalAmount; // Move return statement outside of the loop
-        }
+  const getTotalCartItems = () => {
+    let totalItems = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        totalItems += cartItems[item];
+      }
+    }
+    return totalItems; // Move return statement outside of the loop
+  };
 
-        const getTotalCartItems = () => {
-            let totalItems = 0;
-            for (const item in cartItems) {
-                if (cartItems[item] > 0) {
-                    totalItems += cartItems[item];
-                }
-            }
-            return totalItems; // Move return statement outside of the loop
-        }
+  const contextValue = {
+    getTotalCartItems,
+    getTotalCartAmount,
+    all_parties,
+    cartItems,
+    addToCart,
+    removeFromCart,
+    user,
+    setUser,
+  };
 
-    
-    const contextValue = {getTotalCartItems,getTotalCartAmount,all_parties,cartItems,addToCart,removeFromCart};
-
-    return (
-        <ShopContext.Provider value={contextValue}>
-            {props.children}
-        </ShopContext.Provider>
-    );
-}
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
 
 export default ShopContextProvider;
--- tpo/src/Pages/Partys.jsx
+++ tpo/src/Pages/Partys.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
-import { useContext } from 'react';
-import { ShopContext } from '../Context/ShopContext';
-import { useParams } from 'react-router-dom';
-import Breadcrum from '../Components/Breadcrums/Breadcrum';
-import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
-import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
-import RelatedPartys from '../Components/RelatedPartys/RelatedPartys';
+import React from "react";
+import { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
+import { useParams } from "react-router-dom";
+import Breadcrum from "../Components/Breadcrums/Breadcrum";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
+import DescriptionBox from "../Components/DescriptionBox/DescriptionBox";
+import RelatedPartys from "../Components/RelatedPartys/RelatedPartys";
 
 const Partys = () => {
-    const {all_parties} = useContext(ShopContext);
-    const {partyId} = useParams();
-    const party = all_parties.find((e)=> e.id === Number(partyId));
-    return ( 
-        <div>
-            <Breadcrum party={party}/>
-            <ProductDisplay party = {party}/>
-            <DescriptionBox/>
-            <RelatedPartys/>
-        </div>
-     );
-}
- 
+  const { all_parties, user } = useContext(ShopContext);
+  const { partyId } = useParams();
+  const party = all_parties.find((e) => e.id === Number(partyId));
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  console.log(user);
+
+  return (
+    <div>
+      <Breadcrum party={party} />
+      <ProductDisplay party={party} />
+      <DescriptionBox />
+      <RelatedPartys />
+    </div>
+  );
+};
+
 export default Partys;
