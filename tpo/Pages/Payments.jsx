af297c3936b3c73f0617756e50de8bdefa2501d6
maxirosso <129107270+maxirosso@users.noreply.github.com>
2024-04-20 16:19:35 -0300

    Delete tpo/Pages directory

--- tpo/Pages/Payments.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Payments = () => {
-  return (
-    <div>{/** hacer el pago aca y luego redireccionar a pago exitoso */}</div>
-  );
-};
-
-export default Payments;
