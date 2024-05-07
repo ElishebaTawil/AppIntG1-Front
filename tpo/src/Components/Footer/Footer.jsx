e01265f6dd78a57cc6d3df6652cc592ad0935e1c
TadeoPinque <108435692+TadeoP@users.noreply.github.com>
2024-05-07 01:33:06 -0300

    Cambios footer

--- tpo/src/Components/Footer/Footer.jsx
+++ tpo/src/Components/Footer/Footer.jsx
@@ -11,23 +11,23 @@ const Footer = () => {
     <MDBFooter className='bg-dark text-center text-white'>
       <MDBContainer className='p-4 pb-0'>
         <section className='mb-4'>
-          <MDBBtn outline color="light" floating className='m-1' href='https://www.facebook.com/' target="_blank" role='button'>
+          <MDBBtn outline color="light" floating className='m-1' href='https://www.facebook.com/profile.php?id=61559052163323' target="_blank" role='button'>
             <MDBIcon fab icon='facebook-f' />
           </MDBBtn>
 
-          <MDBBtn outline color="light" floating className='m-1' href='https://twitter.com/' target="_blank" role='button'>
+          <MDBBtn outline color="light" floating className='m-1' href='https://twitter.com/FestTicketTrust' target="_blank" role='button'>
             <MDBIcon fab icon='twitter' />
           </MDBBtn>
 
           <MDBBtn outline color="light" floating className='m-1' href='#' target="_blank" role='button'>
             <MDBIcon class="far fa-envelope"/> {/* Este podria enviarte a una pagina en la cual nos pueda contar su experiencia  */}
           </MDBBtn>
-          <MDBBtn outline color="light" floating className='m-1' href='https://www.instagram.com/?hl=en' target="_blank" role='button'>
+          <MDBBtn outline color="light" floating className='m-1' href='https://www.instagram.com/festtickets11/' target="_blank" role='button'>
             <MDBIcon fab icon='instagram' />
           </MDBBtn>
 
-          <MDBBtn outline color="light" floating className='m-1' href='https://www.linkedin.com/?trk=Officekey' target="_blank"  role='button'>
-            <MDBIcon fab icon='linkedin-in' />
+          <MDBBtn outline color="light" floating className='m-1' href='https://www.youtube.com/' target="_blank"  role='button'>
+            <MDBIcon fab icon='youtube' />
           </MDBBtn>
 
           <MDBBtn outline color="light" floating className='m-1' href='https://github.com/ElishebaTawil/AppIntG1-Front' target="_blank" role='button'>
